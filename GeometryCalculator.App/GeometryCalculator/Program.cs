1a2db9aeb5461d571a18c67b1f449265ef7b771e
Roman Shainov <roman.shainov.work@gmail.com>
2023-06-25 16:35:10 +0200

    Добавлено конспольное приложение с примером вычисление площади фигуры без знания типа фигуры в compile-time

---
+++ GeometryCalculator.App/GeometryCalculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using GeometryLibrary;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+        Console.Write("Введите наименование типа в формате namespace.typeName: ");
+        var typeName = Console.ReadLine();
+        var type = Type.GetType(typeName + $", {nameof(GeometryLibrary)}");
+        if (type != null)
+        {
+            var figure = (Figure)Activator.CreateInstance(type);
+
+            if (figure != null)
+            {
+                Console.WriteLine(figure.ToString());
+            }
+        }
+        else
+        {
+            Console.WriteLine("Неверный тип");
+        }
+
+        Console.ReadKey();
+    }
+}
